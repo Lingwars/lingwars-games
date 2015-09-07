899d500d91d8163a6976da57d66cb63a81242be8
Javier G. Sogo <jgsogo@gmail.com>
2015-09-07 12:42:26 +0200

    comment

--- appweb/appweb/settings.py
+++ appweb/appweb/settings.py
@@ -49,7 +49,7 @@ LINGWARS_GAMES_DIRS = [
     os.path.join(BASE_DIR, '../games'),
 ]
 for item in LINGWARS_GAMES_DIRS:
-    print(item)
+    # TODO: Don't like this...
     sys.path.append(item)
 
 
