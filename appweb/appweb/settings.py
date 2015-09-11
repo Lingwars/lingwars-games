6cb3c3b2892febc9d78a06cc0c10192f8541b407
jgsogo <jgsogo@incendios.gig.etsii.upm.es>
2015-09-11 14:34:49 +0200

    middleware for localization

--- appweb/appweb/settings.py
+++ appweb/appweb/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'appweb.urls'
--- games/word2def/views.py
+++ games/word2def/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from django.conf import settings
 from django.core.urlresolvers import reverse
