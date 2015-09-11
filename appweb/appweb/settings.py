3bc6e9e2e274b6632fc7a3376c5b301cc535076e
Javier G. Sogo <jgsogo@gmail.com>
2015-09-11 13:33:34 +0200

    only es|en

--- appweb/appweb/settings.py
+++ appweb/appweb/settings.py
@@ -101,7 +101,11 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
+LANGUAGES = [
+    ('en', 'English'),
+    ('es', 'Spanish'),
+    ]
 
 TIME_ZONE = 'UTC'
 
